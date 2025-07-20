98c8cff0fb0e3c0e9254e5d2d58522362cd89022
Faheem <faheemrayyan17@gmail.com>
2025-07-20 16:38:51 +0800

    Create Nrkv4Tsb.lua

---
+++ Nrkv4Tsb.lua
@@ -0,0 +1,54 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "NRK v4 | The strongest Battlegrounds",
+   Icon = 1049060234, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "😃 The Script its loading...",
+   LoadingSubtitle = "🤍 Made By Faheem & k4sii Checking Valiadating..",
+   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "L", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = NrkV4, -- Create a custom folder for your hub/game
+      FileName = "BOmtopHub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Athencation | Powered by Boba.gg",
+      Subtitle = "🔑 Key System",
+      Note = "Please enter the valid key!", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"NRK101"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local Tab = Window:CreateTab("Info", "info")
+local Tab = Window:CreateTab("Main", "code")
+local Tab = Window:CreateTab("Misc", "folder-code")
+local Tab = Window:CreateTab("Enchanced+", "moon-star")
+
+local Paragraph = Info:CreateParagraph({Title = "❗️Information", Content = "This Hub its still in (BETA!)"})
+
+local Paragraph = Info:CreateParagraph({Title = "🤍 Credits:", Content = "Faheem: Owner-Script YQANTQ: Script function callback Subcribe to him for more script on yt⚙️"})
+
+local Label = Tab:CreateLabel("Faheem", 9676276958, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, IgnoreTheme
+
+local Label = Tab:CreateLabel("Mspaint - Upio", 6675147490, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, IgnoreTheme
+
+
+
